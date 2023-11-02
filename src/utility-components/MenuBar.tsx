3c22d16f5e7d12db5e1b38077d29aefb46bf8f30
Gunnar Ingi Stefánsson <gstefansson@esri.com>
2023-11-02 11:21:47 +0100

    small improvements to the menu bar styling

--- src/utility-components/MenuBar.tsx
+++ src/utility-components/MenuBar.tsx
@@ -1,6 +1,6 @@
 import { tsx } from "@arcgis/core/widgets/support/widget";
-import styles from './menu-bar.module.scss'
-import cx from 'classnames';
+import styles from "./menu-bar.module.scss";
+import cx from "classnames";
 import { type Page } from "../application/AppState";
 import { PointerEventsContainer } from "./PointerEventsContainer";
 
@@ -8,47 +8,72 @@ interface MenuBarProps {
   onItemClick: (item: Page) => void;
 }
 export function MenuBar({ onItemClick }: MenuBarProps) {
-
   return (
-    <PointerEventsContainer children={(
-      <nav class={styles.menu}>
-        <a
-          class={cx(styles.menuItem, styles.explore)}
-          onclick={(e: Event) => { onItemClick('landing') }}
-        >
-          <img class={cx(styles.icon, styles.exploreLogo)} src="../images/logo.png" alt="Explore Mars" />
-        </a>
-        <div class={styles.items}>
+    <PointerEventsContainer
+      children={
+        <nav class={styles.menu}>
           <a
-            class={cx(styles.menuItem, styles.location)}
-            onclick={(e: Event) => { onItemClick('locations') }}
+            class={cx(styles.menuItem, styles.logoItem, styles.explore)}
+            onclick={(e: Event) => {
+              onItemClick("landing");
+            }}
           >
-            <img class={styles.icon} alt="" src="../images/location.svg" />
-            <div class={styles.label}>Locations</div>
+            <img
+              class={cx(styles.icon, styles.exploreLogo)}
+              src="../images/logo.png"
+              alt="Explore Mars"
+            />
+            <img
+              class={cx(styles.icon, styles.exploreLogoHighlighted)}
+              src="../images/logo_highlighted.png"
+              alt=""
+            />
           </a>
+          <div class={styles.items}>
+            <a
+              class={cx(styles.menuItem, styles.location)}
+              onclick={(e: Event) => {
+                onItemClick("locations");
+              }}
+            >
+              <img class={styles.icon} alt="" src="../images/location.svg" />
+              <div class={styles.label}>Locations</div>
+            </a>
+            <a
+              class={cx(styles.menuItem)}
+              onclick={(e: Event) => {
+                onItemClick("measure");
+              }}
+            >
+              <img class={styles.icon} alt="" src="../images/measure.svg" />
+              <div class={styles.label}>Measure</div>
+            </a>
+            <a
+              class={cx(styles.menuItem, styles.compare)}
+              onclick={(e: Event) => {
+                onItemClick("compare");
+              }}
+            >
+              <img class={styles.icon} alt="" src="../images/compare.svg" />
+              <div class={styles.label}>Compare</div>
+            </a>
+          </div>
           <a
-            class={cx(styles.menuItem)}
-            onclick={(e: Event) => { onItemClick('measure') }}
+            class={cx(styles.menuItem, styles.logoItem, styles.credits)}
+            onclick={(e: Event) => {
+              onItemClick("credits");
+            }}
           >
-            <img class={styles.icon} alt="" src="../images/measure.svg" />
-            <div class={styles.label}>Measure</div>
+            <img
+              class={cx(styles.icon, styles.esriLogo)}
+              src="../images/esri-logo.png"
+              title="Credits"
+              alt=""
+            />
+            <div class={styles.label}>ArcGIS API for Javascript</div>
           </a>
-          <a
-            class={cx(styles.menuItem, styles.compare)}
-            onclick={(e: Event) => { onItemClick('compare') }}
-          >
-            <img class={styles.icon} alt="" src="../images/compare.svg" />
-            <div class={styles.label}>Compare</div>
-          </a>
-        </div>
-        <a
-          class={cx(styles.menuItem, styles.credits)}
-          onclick={(e: Event) => { onItemClick('credits') }}
-        >
-          <img class={cx(styles.icon, styles.esriLogo)} src="../images/esri-logo.png" title="Credits" alt="" />
-          <div class={styles.label}>ArcGIS API for Javascript</div>
-        </a>
-      </nav>
-    )}/>
-  )
+        </nav>
+      }
+    />
+  );
 }
