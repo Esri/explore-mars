62495d969a2bf5fb9d12ffccacf8b0ca3a0b3b5d
Gunnar Ingi Stefánsson <gstefansson@esri.com>
2023-11-23 10:45:58 +0100

    goes back to main menu when closing a tool page

--- src/utility-components/CloseButton.tsx
+++ src/utility-components/CloseButton.tsx
@@ -1,10 +1,19 @@
 import { tsx } from "@arcgis/core/widgets/support/widget";
 import styles from "./close-button.module.scss";
+import AppState from "../application/AppState";
 
 interface CloseButtonProps {
-  onClose: () => void;
+  onClose?: () => void;
 }
 
 export function CloseButton({ onClose }: CloseButtonProps) {
-  return <button class={styles.closeButton} onclick={onClose} />;
+  return (
+    <button
+      class={styles.closeButton}
+      onclick={() => {
+        onClose?.();
+        AppState.page = "home";
+      }}
+    />
+  );
 }
