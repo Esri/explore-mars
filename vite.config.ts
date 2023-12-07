23f25069dc9b63dbf65903d9bfb55981e49eafbb
Gunnar Ingi Stefánsson <gstefansson@esri.com>
2023-12-07 10:42:42 +0100

    sets up pipeline to optimize the gltf assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@
 // @ts-expect-error the gltf plugin does not have type definitions
 import gltf from "vite-plugin-gltf";
 import { defineConfig } from "vite";
+import {
+  dedup,
+  draco,
+  prune,
+  textureCompress,
+} from "@gltf-transform/functions";
+import sharp from "sharp";
 
 export default defineConfig({
   server: {
@@ -11,5 +18,17 @@ export default defineConfig({
   build: {
     target: "es2020",
   },
-  plugins: [gltf()],
+  plugins: [
+    gltf({
+      transforms: [
+        // remove unused resources
+        prune(),
+        // combine duplicated resources
+        dedup(),
+        textureCompress({ encoder: sharp, resize: [2048, 2048] }),
+        // compress mesh geometry
+        draco(),
+      ],
+    }),
+  ],
 });
